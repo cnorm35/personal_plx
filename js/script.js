e0e344313788c6ec4e55d064907a796688f9f071
Cody Norman <wcodynorman@gmail.com>
2014-08-17 12:08:07 -0700

    added scrolling animations

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
+	$('.animate-content').addClass('hidden').viewportChecker({
+		classToAdd: 'visible animated fadeIn',
+		offset: 100
+	});
+
 	$objWindow = $(window);
 	$('div[data-type="background"]').each(function(){
 		var $bgObj = $(this);
@@ -10,4 +15,6 @@ $(document).ready(function(){
 			$bgObj.css({backgorundPosition: coords});
 		});
 	});
+
+	
 });
